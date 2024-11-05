c99580c0a446025f8519e87c112f9926561c909d
cragtok <lidfun@protonmail.com>
2024-11-05 15:17:03 +0600

    restore back to working version

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,6 @@ app.use(middleware.requestLogger);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("build"));
     app.get("/health", (req, res) => {
-        // eslint-disable-next-line no-constant-condition
-        if (true) throw "error...  ";
         res.send("ok");
     });
     app.get("/*", function(req, res, next) {
